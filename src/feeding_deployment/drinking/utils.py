f5a4a53616072c86a5ac91102bc67bdff418e02a
Tom Silver <tomssilver@gmail.com>
2024-08-19 11:46:20 -0400

    add py.typed

--- src/feeding_deployment/drinking/utils.py
+++ src/feeding_deployment/drinking/utils.py
@@ -13,7 +13,7 @@ from pybullet_helpers.inverse_kinematics import (
 from pybullet_helpers.joint import (
     JointPositions,
 )
-from scene import (
+from feeding_deployment.drinking.scene import (
     CupManipulationSceneDescription,
     CupManipulationSceneIDs,
 )
