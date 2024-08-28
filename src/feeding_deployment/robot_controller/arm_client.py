63be67815110ec3803cdc0c0be9a659cff395b9c
RKJenamani <rajatkj11@gmail.com>
2024-08-28 11:01:45 -0400

    switching to separate interface class for arm control

--- src/feeding_deployment/robot_controller/arm_client.py
+++ src/feeding_deployment/robot_controller/arm_client.py
@@ -1,188 +1,23 @@
-# This RPC server allows other processes to communicate with the Kinova arm
-# low-level controller, which runs in its own, dedicated real-time process.
-#
-# Note: Operations that are not time-sensitive should be run in a separate,
-# non-real-time process to avoid interfering with the real-time low-level
-# control and causing latency spikes.
-
-import queue
 import threading
 import time
-
 import numpy as np
-from multiprocess.managers import BaseManager as MPBaseManager
 
-# from arm_controller import JointCompliantController
-# from constants import RPC_AUTHKEY, ARM_RPC_PORT
-RPC_AUTHKEY = b"secret-key"
-ARM_RPC_PORT = 5000
-# import rospy
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Pose
 
-# from ik_solver import IKSolver
-from feeding_deployment.robot_controller.command_interface import KinovaCommand
-
-try:
-    from feeding_deployment.robot_controller.kinova import KinovaArm
-except ImportError:
-    print(
-        "KinovaArm import failed, continuing without executing arm commands on real robot"
-    )
-
-# from sensor_msgs.msg import JointState
-
-NUC_HOSTNAME = "192.168.1.3"
-
-
-class Arm:
-    def __init__(self):
-        self.arm = KinovaArm()
-        # self.arm.set_joint_limits(speed_limits=(7 * (30,)), acceleration_limits=(7 * (80,)))
-        self.command_queue = queue.Queue(1)
-        self.controller = None
-
-    def get_state(self):
-        arm_pos, ee_pose, gripper_pos = self.arm.get_state()
-        return arm_pos, ee_pose, gripper_pos
-    
-    def get_update_state(self):
-        arm_pos, arm_vel, gripper_pos = self.arm.get_update_state()
-        return arm_pos, arm_vel, gripper_pos
-
-    def reset(self):
-        # Go to home position
-        print("Moving to home position")
-        self.arm.home()
-
-    def switch_to_joint_compliant_mode(self):
-
-        # clear command queue
-        print("Clearing command queue")
-        while not self.command_queue.empty():
-            self.command_queue.get()
-
-        # switch to joint compliant mode
-        print("Switching to joint compliant mode")
-        self.arm.switch_to_joint_compliant_mode(self.command_queue)
-
-    def switch_out_of_joint_compliant_mode(self):
-        # switch out of joint compliant mode
-        print("Switching out of joint compliant mode")
-        self.arm.switch_out_of_joint_compliant_mode()
-
-    def compliant_set_joint_position(self, command_pos):
-        print(f"Received compliant joint pos command: {command_pos}")
-        gripper_pos = 0
-        self.command_queue.put((command_pos, gripper_pos))
-
-    def compliant_set_joint_trajectory(self, trajectory_command):
-        print(
-            f"Received compliant joint trajectory command with {len(trajectory_command)} waypoints"
-        )
-        gripper_pos = 0
-        for command_pos in trajectory_command:
-            while True:
-                time.sleep(0.01)
-                q, _, _ = self.arm.get_update_state()
-                error = np.linalg.norm(np.array(command_pos) - np.array(q))
-                # threshold = 0.03*np.sqrt(7)
-                threshold = 0.3
-                print(f"Error: {error}, Threshold: {threshold}")
-                # When near (distance < threshold) next waypoint, update to next waypoint
-                if error < threshold:
-                    self.command_queue.put((command_pos, gripper_pos))
-                    break
-
-    # def compliant_set_joint_trajectory(self, trajectory_command):
-    #     print(
-    #         f"Received compliant joint trajectory command with {len(trajectory_command)} waypoints"
-    #     )
-    #     gripper_pos = 0
-    #     for command_pos in trajectory_command:
-    #         self.command_queue.put((command_pos, gripper_pos))
-    #         time.sleep(0.1)
-
-    def set_joint_position(self, command_pos):
-        print(f"Received joint pos command: {command_pos}")
-        self.arm.move_angular(command_pos)
-
-    def set_joint_trajectory(self, trajectory_command):
-        print(
-            f"Received joint trajectory command with {len(trajectory_command)} waypoints"
-        )
-        self.arm.move_angular_trajectory(trajectory_command)
-
-    def set_ee_pose(self, xyz, xyz_quat):
-        print(f"Received cartesian pose command: {xyz}, {xyz_quat}")
-        self.arm.move_cartesian(xyz, xyz_quat)
-
-    def set_gripper(self, gripper_pos):
-        print(f"Received gripper pos command: {gripper_pos}")
-        self.arm._gripper_position_command(gripper_pos)
-
-    def open_gripper(self):
-        print("Received open gripper command")
-        self.arm.open_gripper()
-
-    def close_gripper(self):
-        print("Received close gripper command")
-        self.arm.close_gripper()
-
-    def close(self):
-        print("Closing arm connection")
-        self.arm.disconnect()
-
-    def retract(self):
-        self.arm.retract()
-
-    def stop(self):
-        print("Stopping arm")
-        self.arm.stop()
-
-    def execute_command(self, cmd: KinovaCommand) -> None:
-
-        if cmd.__class__.__name__ == "JointTrajectoryCommand":
-            return self.set_joint_trajectory(cmd.traj)
-
-        if cmd.__class__.__name__ == "JointCommand":
-            return self.set_joint_position(cmd.pos)
-
-        if cmd.__class__.__name__ == "CartesianCommand":
-            return self.set_ee_pose(cmd.pos, cmd.quat)
-
-        if cmd.__class__.__name__ == "OpenGripperCommand":
-            return self.open_gripper()
-
-        if cmd.__class__.__name__ == "CloseGripperCommand":
-            return self.close_gripper()
-
-        raise NotImplementedError(f"Unrecognized command: {cmd}")
-
-
-class ArmManager(MPBaseManager):
-    pass
-
-
-ArmManager.register("Arm", Arm)
+from feeding_deployment.robot_controller.arm_interface import ArmInterface, ArmManager, NUC_HOSTNAME, ARM_RPC_PORT, RPC_AUTHKEY
 
 if __name__ == "__main__":
-    # manager = ArmManager(address=(NUC_HOSTNAME, ARM_RPC_PORT), authkey=RPC_AUTHKEY)
-    # server = manager.get_server()
-    # print(f'Arm manager server started at {NUC_HOSTNAME}:{ARM_RPC_PORT}')
-    # server.serve_forever()
-    import numpy as np
 
     # from constants import POLICY_CONTROL_PERIOD
     POLICY_CONTROL_PERIOD = 0.1
     manager = ArmManager(address=(NUC_HOSTNAME, ARM_RPC_PORT), authkey=RPC_AUTHKEY)
     manager.connect()
-    arm = manager.Arm()
+    arm = manager.ArmInterface()
     joint_state_thread = None
     try:
-        import rospy
-        from sensor_msgs.msg import JointState
-        from std_msgs.msg import Bool
-        from geometry_msgs.msg import Pose
-
         rospy.init_node("arm_client", anonymous=True)
 
         def emergency_stop_callback(msg):
@@ -327,7 +162,7 @@ if __name__ == "__main__":
         input("Press Enter to move to before transfer pose...")
         arm.set_joint_position(before_transfer_pos)
 
-        # print("Current Arm State:", arm.get_state())
+        # print("Current ArmInterface State:", arm.get_state())
 
         # transfer_pose = [-2.76117261, -1.18670831, -1.7014329 , -1.81186993,  0.26973719, -0.09092458,  2.4944019]
         # input("Press Enter to set the arm to transfer pose")
