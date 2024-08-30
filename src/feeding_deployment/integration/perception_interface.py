fe536818efe336d60241c899835fce0b58468377
RKJenamani <rajatkj11@gmail.com>
2024-08-30 13:22:43 -0400

    watchdog works

--- src/feeding_deployment/integration/perception_interface.py
+++ src/feeding_deployment/integration/perception_interface.py
@@ -15,60 +15,19 @@ try:
 except ModuleNotFoundError:
     pass
 
-from feeding_deployment.robot_controller.arm_interface import ArmInterface
-
-
-def publish_joint_states(arm):
-
-    # publish joint states
-    joint_states_pub = rospy.Publisher("/robot_joint_states", JointState, queue_size=10)
-
-    while not rospy.is_shutdown():
-        arm_pos, ee_pose, gripper_pos = arm.get_state()
-        joint_state_msg = JointState()
-        joint_state_msg.header.stamp = rospy.Time.now()
-        joint_state_msg.name = [
-            "joint_1",
-            "joint_2",
-            "joint_3",
-            "joint_4",
-            "joint_5",
-            "joint_6",
-            "joint_7",
-            "finger_joint",
-        ]
-        joint_state_msg.position = arm_pos.tolist() + [gripper_pos]
-        joint_state_msg.velocity = [0.0] * 8
-        joint_state_msg.effort = [0.0] * 8
-        joint_states_pub.publish(joint_state_msg)
-        time.sleep(0.01)
-
+from feeding_deployment.robot_controller.arm_client import ArmInterfaceClient
 
 class PerceptionInterface:
     """An interface for perception (robot joints, human head poses, etc.)."""
 
-    def __init__(self, robot_interface: ArmInterface | None) -> None:
+    def __init__(self, robot_interface: ArmInterfaceClient | None) -> None:
         self._robot_interface = robot_interface
-
-        try:
-            rospy.init_node("perception_interface", anonymous=True)
-
-        except Exception:
-            pass
-
-        # publish joint states in separate thread
-        if robot_interface is not None:
-            joint_state_thread = threading.Thread(
-                target=publish_joint_states, args=(self._robot_interface,)
-            )
-            joint_state_thread.start()
 
         # run head perception
         if robot_interface is None:
             self._head_perception = None
         else:
             self._head_perception = None
-            # RemoveBeforePushToDo
             # self._head_perception = HeadPerceptionROSWrapper()
             # # warm start head perception
             # for _ in range(10):
@@ -76,8 +35,10 @@ class PerceptionInterface:
 
     def get_robot_joints(self) -> "JointState":
         """Get the current robot joint state."""
-        q, x, gripper_position = self._robot_interface.get_state()
-        gripper_position *= 0.8 # sim + joint_lims consider closed gripper as 0.8 
+        joint_state_msg = rospy.wait_for_message("/robot_joint_states", JointState)
+        q = np.array(joint_state_msg.position[:7])
+        gripper_position = joint_state_msg.position[7]
+        
         joint_state = q.tolist() + [
             gripper_position,
             gripper_position,
--- src/feeding_deployment/safety/watchdog.py
+++ src/feeding_deployment/safety/watchdog.py
@@ -1,5 +1,5 @@
 '''
-This script is a client-side (run on compute machine) watchdog for the robot's intended functionality. 
+Runs a client-side (run on compute machine) watchdog for the robot's intended functionality. 
 It validates the following:
 1. All sensors are streaming correctly.
 2. All sensor outputs are within the expected range.
@@ -20,16 +20,27 @@ from sensor_msgs.msg import CameraInfo
 from geometry_msgs.msg import WrenchStamped
 from std_msgs.msg import Bool
 
+import threading
+import time
+import numpy as np
+
+import rospy
+from std_msgs.msg import Bool
+from netft_rdt_driver.srv import String_cmd
+
+from feeding_deployment.robot_controller.arm_interface import ArmInterface, ArmManager, NUC_HOSTNAME, ARM_RPC_PORT, RPC_AUTHKEY
+
 CAMERA_FREQUENCY_THRESHOLD = 20 # expected is 30 Hz
 FT_FREQUENCY_THRESHOLD = 800 # expected is 1000 Hz
 FT_THRESHOLD = [10.0, 10.0, 10.0, 0.5, 0.5, 0.5]
 COLLISION_FREE_FREQUENCY_THRESHOLD = 300 # expected is 350 Hz (empirical)
 USER_ESTOP_FREQUENCY_THRESHOLD = 800 # expected is 1000 Hz
-EXPERIMENTOR_ESTOP_FREQUENCY_THRESHOLD = 800 # expected is 1000 Hz
+experimentor_ESTOP_FREQUENCY_THRESHOLD = 800 # expected is 1000 Hz
 
-WATCHDOG_MONITOR_FREQUENCY = 1000
+WATCHDOG_RUN_FREQUENCY = 1000
 
 class AnomalyStatus(Enum):
+    UNEXPECTED_ERROR = -1
     NO_ANOMALY = 0
     CAMERA_FREQUENCY = 1
     CAMERA_UNEXPECTED = 2
@@ -39,8 +50,8 @@ class AnomalyStatus(Enum):
     COLLISION_FREE_UNEXPECTED = 6
     USER_ESTOP_FREQUENCY = 7
     USER_ESTOP_PRESSED = 8
-    EXPERIMENTOR_ESTOP_FREQUENCY = 9
-    EXPERIMENTOR_ESTOP_PRESSED = 10
+    experimentor_ESTOP_FREQUENCY = 9
+    experimentor_ESTOP_PRESSED = 10
 
 class PeekableQueue(queue.Queue):
     def peek(self):
@@ -48,31 +59,49 @@ class PeekableQueue(queue.Queue):
             if len(self.queue) > 0:
                 return self.queue[0]  # Safely access the first element
             else:
-                raise queue.Empty  # Handle the case when the queue is empty
+                return float('inf') # Handle the case when the queue is empty: Do not pop from an empty queue
 
 class WatchDog:
     def __init__(self):
 
-        self.camera_info_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.cameraCallback)
+        # Register ArmInterface (no lambda needed on the client-side)
+        ArmManager.register("ArmInterface")
+
+        # Client setup
+        self.manager = ArmManager(address=(NUC_HOSTNAME, ARM_RPC_PORT), authkey=RPC_AUTHKEY)
+        self.manager.connect()
+
+        # This will now use the single, shared instance of ArmInterface
+        self._arm_interface = self.manager.ArmInterface()
+
+        # bias FT sensor
+        bias = rospy.ServiceProxy('/forque/bias_cmd', String_cmd)
+        bias('bias')
+
+        queue_size = 1000
+        self.camera_info_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.cameraCallback, queue_size = queue_size, buff_size = 65536*queue_size)
         self.camera_timestamps = PeekableQueue()
         self.camera_unexpected = False # ToDo - Implement this
         
-        self.ft_sub = rospy.Subscriber('/forque/forqueSensor', WrenchStamped, self.ftCallback)
+        self.ft_sub = rospy.Subscriber('/forque/forqueSensor', WrenchStamped, self.ftCallback, queue_size = queue_size, buff_size = 65536*queue_size)
         self.ft_timestamps = PeekableQueue()
         self.ft_unexpected = False
 
-        self.collision_free_sub = rospy.Subscriber('/collision_free', Bool, self.collisionFreeCallback)
+        self.collision_free_sub = rospy.Subscriber('/collision_free', Bool, self.collisionFreeCallback, queue_size = queue_size, buff_size = 65536*queue_size)
         self.collision_free_timestamps = PeekableQueue()
         self.collision_free_unexpected = False
 
-        self.user_emergency_stop_sub = rospy.Subscriber('/user_estop', Bool, self.userEmergencyStopCallback)
+        self.user_emergency_stop_sub = rospy.Subscriber('/user_estop', Bool, self.userEmergencyStopCallback, queue_size = queue_size, buff_size = 65536*queue_size)
         self.user_emergency_stop_timestamps = PeekableQueue()
         self.user_emergency_stop_pressed = False
 
-        self.experimentor_emergency_stop_sub = rospy.Subscriber('/experimentor_estop', Bool, self.experimentorEmergencyStopCallback)
-        self.exeperimentor_emergency_stop_timestamps = PeekableQueue()
+        self.experimentor_emergency_stop_sub = rospy.Subscriber('/experimentor_estop', Bool, self.experimentorEmergencyStopCallback, queue_size = queue_size, buff_size = 65536*queue_size)
+        self.experimentor_emergency_stop_timestamps = PeekableQueue()
         self.experimentor_emergency_stop_pressed = False
 
+        self.watchdog_status_pub = rospy.Publisher("/watchdog_status", Bool, queue_size=1)
+
+        self.second_counter = 0
         time.sleep(2.0) # Wait for all queues to fill up
         print("Initialized.")
 
@@ -105,43 +134,66 @@ class WatchDog:
 
     def experimentorEmergencyStopCallback(self, msg):
 
-        self.exeperimentor_emergency_stop_timestamps.put(time.time())
+        self.experimentor_emergency_stop_timestamps.put(time.time())
         if msg.data:
             self.experimentor_emergency_stop_pressed = True
 
-
-    def run(self):
+    def check_status(self):
+        self.second_counter += 1
         anomaly = AnomalyStatus.NO_ANOMALY
-
         start_time = time.time()
+        frequencies = []
         for _queue, _threshold, _anomaly in [(self.camera_timestamps, CAMERA_FREQUENCY_THRESHOLD, AnomalyStatus.CAMERA_FREQUENCY), 
                                             (self.ft_timestamps, FT_FREQUENCY_THRESHOLD, AnomalyStatus.FT_FREQUENCY), 
                                             (self.collision_free_timestamps, COLLISION_FREE_FREQUENCY_THRESHOLD, AnomalyStatus.COLLISION_FREE_FREQUENCY), 
                                             (self.user_emergency_stop_timestamps, USER_ESTOP_FREQUENCY_THRESHOLD, AnomalyStatus.USER_ESTOP_FREQUENCY), 
-                                            (self.exeperimentor_emergency_stop_timestamps, EXPERIMENTOR_ESTOP_FREQUENCY_THRESHOLD, AnomalyStatus.EXPERIMENTOR_ESTOP_FREQUENCY)]:
+                                            (self.experimentor_emergency_stop_timestamps, experimentor_ESTOP_FREQUENCY_THRESHOLD, AnomalyStatus.experimentor_ESTOP_FREQUENCY)]:
             while _queue.peek() < start_time - 1.0:
                 _queue.get()
-            if _queue.qsize() < _threshold:
+            queue_size = _queue.qsize()
+            if queue_size < _threshold:
+                print(f"Frequency: {queue_size} for {_anomaly}")
                 anomaly = _anomaly
                 break   
+            frequencies.append(queue_size)
+
+        if self.second_counter == WATCHDOG_RUN_FREQUENCY:
+            print("Watchdog running at expected frequency.")
+            print(f"Frequencies:  Camera: {frequencies[0]}, FT: {frequencies[1]}, Collision Free: {frequencies[2]}, User EStop: {frequencies[3]}, Experimentor EStop: {frequencies[4]}")
+            self.second_counter = 0
 
         for _unexpected, _anomaly in [(self.camera_unexpected, AnomalyStatus.CAMERA_UNEXPECTED),
                                     (self.ft_unexpected, AnomalyStatus.FT_UNEXPECTED),
                                     (self.collision_free_unexpected, AnomalyStatus.COLLISION_FREE_UNEXPECTED),
                                     (self.user_emergency_stop_pressed, AnomalyStatus.USER_ESTOP_PRESSED),
-                                    (self.experimentor_emergency_stop_pressed, AnomalyStatus.EXPERIMENTOR_ESTOP_PRESSED)]:
+                                    (self.experimentor_emergency_stop_pressed, AnomalyStatus.experimentor_ESTOP_PRESSED)]:
             if _unexpected:
+                print(f"Unexpected: {_anomaly}")
                 anomaly = _anomaly
                 break
 
+        if anomaly != AnomalyStatus.NO_ANOMALY:
+            print(f"AnomalyStatus detected: {anomaly}")
+            self._arm_interface.stop()
+            self._arm_interface.close()
+
+        self.watchdog_status_pub.publish(Bool(data=anomaly == AnomalyStatus.NO_ANOMALY))
         return anomaly
+    
+    def run(self):
+        while True:
+            start_time = time.time()
+            status = self.check_status()
+            if status != AnomalyStatus.NO_ANOMALY:
+                print(f"AnomalyStatus detected: {status}")
+                break
+            end_time = time.time()
+            # print(f"Time taken: {end_time - start_time}")
+            time.sleep(max(0, 1.0/WATCHDOG_RUN_FREQUENCY - (end_time - start_time)))
 
 if __name__ == '__main__':
 
     rospy.init_node('WatchDog', anonymous=True)
     watchdog = WatchDog()
-    while True:
-        status = watchdog.run()
-        if status != AnomalyStatus.NO_ANOMALY:
-            print(f"AnomalyStatus detected: {status}")
-            break
+    watchdog.run()
+    
