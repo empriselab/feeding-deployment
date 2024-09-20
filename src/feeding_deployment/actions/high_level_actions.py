82ae25ab207fb53c8f3f0778ce0ac5c3eb587f30
RKJenamani <rajatkj11@gmail.com>
2024-09-20 19:51:48 -0400

    demo with Tapo

--- src/feeding_deployment/actions/high_level_actions.py
+++ src/feeding_deployment/actions/high_level_actions.py
@@ -882,7 +882,8 @@ class LookAtPlateHLA(HighLevelAction):
                 items = self.flair.identify_plate(camera_color_data)
                 # flair.set_food_items(items)
                 # self.flair.set_food_items(['banana', 'baby carrot'])
-                self.flair.set_food_items(['cantaloupe', 'banana'])
+                # self.flair.set_food_items(['cantaloupe', 'banana'])
+                self.flair.set_food_items(['chicken nugget'])
                 items_detection = self.flair.detect_items(camera_color_data, camera_depth_data, camera_info_data, log_path=None)
                 
                 if not self._preferences_set:
