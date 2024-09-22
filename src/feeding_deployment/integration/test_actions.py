9e3445671beb0fea16c13f8e49b78619988f5c91
RKJenamani <rajatkj11@gmail.com>
2024-09-22 11:48:00 -0400

    added exception handling to arm interface to avoid pickling stack trace

--- src/feeding_deployment/integration/test_actions.py
+++ src/feeding_deployment/integration/test_actions.py
@@ -197,8 +197,8 @@ def _main(
     # Create skills for high-level planning.
     hla_hyperparams = {"max_motion_planning_time": max_motion_planning_time}
 
-    test_LookAtPlateHLA(sim, robot_interface, perception_interface, rviz_interface, web_interface, hla_hyperparams, run_on_robot, wrist_controller, flair, make_videos)
-    test_AcquireBiteHLA(sim, robot_interface, perception_interface, rviz_interface, web_interface, hla_hyperparams, run_on_robot, wrist_controller, flair, make_videos)
+    # test_LookAtPlateHLA(sim, robot_interface, perception_interface, rviz_interface, web_interface, hla_hyperparams, run_on_robot, wrist_controller, flair, make_videos)
+    # test_AcquireBiteHLA(sim, robot_interface, perception_interface, rviz_interface, web_interface, hla_hyperparams, run_on_robot, wrist_controller, flair, make_videos)
     test_TransferToolHLA(sim, robot_interface, perception_interface, rviz_interface, web_interface, hla_hyperparams, run_on_robot, wrist_controller, flair, make_videos, tool)
 
 if __name__ == "__main__":
