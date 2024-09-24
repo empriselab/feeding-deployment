83fc8d5b7f736aba19e434dbb6457dabcf64355f
RKJenamani <rajatkj11@gmail.com>
2024-09-24 15:12:24 -0400

    updated collision monitor + interface to actually switch to gravity compensation mode within controller

--- src/feeding_deployment/safety/collision_monitor.py
+++ src/feeding_deployment/safety/collision_monitor.py
@@ -36,29 +36,6 @@ class CollisionMonitor:
         self._scene_description = SceneDescription()
         self._sim = FeedingDeploymentPyBulletSimulator(self._scene_description, use_gui=False, ignore_user=True)
 
-        self.joint_limits = {
-            "joint_2": {"lower": -2.24, "upper": 2.24},
-            "joint_4": {"lower": -2.57, "upper": 2.57},
-            "joint_6": {"lower": -2.09, "upper": 2.09},
-        }
-
-        # add padding of 10 degrees to the joint limits
-        padding = 15 * np.pi / 180
-        self.soft_joint_limits = {}
-        for joint_name, limits in self.joint_limits.items():
-            self.soft_joint_limits[joint_name] = {
-                "lower": limits["lower"] + padding,
-                "upper": limits["upper"] - padding,
-            }
-
-        padding = 15 * np.pi / 180
-        self.hard_joint_limits = {}
-        for joint_name, limits in self.joint_limits.items():
-            self.hard_joint_limits[joint_name] = {
-                "lower": limits["lower"] + padding,
-                "upper": limits["upper"] - padding,
-            }
-
     def _joint_state_callback(self, joint_state_msg: "JointState") -> None:
         # Convert joint state message into JointPositions.
         assert joint_state_msg.name == [
@@ -80,33 +57,9 @@ class CollisionMonitor:
                                                             finger_state)
         # Run collision checking.
         has_collision = self.check_collisions(combined_joint_state)
-        outside_soft_joint_limits = self.check_outside_soft_joint_limits(joint_state_msg.name, arm_joint_state)
         print("In Collision" if has_collision else "Collision Free")
-        print("Outside Soft Joint Limits" if outside_soft_joint_limits else "Within Soft Joint Limits")
-        self._collision_pub.publish(Bool(data=not (has_collision)))
-
-        outside_hard_joint_limits = self.check_outside_hard_joint_limits(joint_state_msg.name, arm_joint_state)
-        print("Outside Hard Joint Limits" if outside_hard_joint_limits else "Within Hard Joint Limits")
-        self._within_joint_limits_pub.publish(Bool(data=not outside_hard_joint_limits))
-
-    def check_outside_soft_joint_limits(self, joint_names: list[str], joint_positions: list[float]) -> bool:
-        """Check if the joint positions are within the soft joint limits."""
-        for joint_name, joint_position in zip(joint_names, joint_positions):
-            if joint_name in self.soft_joint_limits:
-                limits = self.soft_joint_limits[joint_name]
-                if joint_position < limits["lower"] or joint_position > limits["upper"]:
-                    return True
-        return False
-
-    def check_outside_hard_joint_limits(self, joint_names: list[str], joint_positions: list[float]) -> bool:
-        """Check if the joint positions are within the hard joint limits."""
-        for joint_name, joint_position in zip(joint_names, joint_positions):
-            if joint_name in self.hard_joint_limits:
-                limits = self.hard_joint_limits[joint_name]
-                if joint_position < limits["lower"] or joint_position > limits["upper"]:
-                    return True
-        return False
-
+        self._collision_pub.publish(Bool(data=not has_collision))
+        
     def check_collisions(self, joint_positions: JointPositions) -> bool:
         """Check collisions, but only with objects that can't be held."""
         collision_ids = self._sim.get_collision_ids()
