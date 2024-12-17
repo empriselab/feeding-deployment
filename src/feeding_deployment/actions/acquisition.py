08c52d07d41165fa15e9c835119ecd2ef977f31f
RKJenamani <rajatkj11@gmail.com>
2024-12-17 15:51:23 -0500

    [WIP] moving to new sim interface + cleaned up actions

---
+++ src/feeding_deployment/actions/acquisition.py
@@ -0,0 +1,304 @@
+from typing import Any
+
+import time
+import pickle
+import numpy as np
+import cv2
+
+from relational_structs import (
+    GroundAtom,
+    GroundOperator,
+    LiftedAtom,
+    LiftedOperator,
+    Object,
+    Predicate,
+    Type,
+    Variable,
+)
+from base import (
+    HighLevelAction,
+    tool_type,
+    GripperFree,
+    Holding,
+    IsUtensil,
+    PlateInView,
+    ToolPrepared
+)
+
+class LookAtPlateHLA(HighLevelAction):
+    """Look at plate in preparation of bite acquisition."""
+
+    def get_name(self) -> str:
+        return "LookAtPlate"
+
+    def get_operator(self) -> LiftedOperator:
+        tool = Variable("?tool", tool_type)
+        return LiftedOperator(
+            self.get_name(),
+            parameters=[tool],
+            preconditions={Holding([tool]), IsUtensil([tool])},
+            add_effects={LiftedAtom(PlateInView, [])},
+            delete_effects=set(),
+        )
+
+    def execute_action(
+        self,
+        objects: tuple[Object, ...],
+        params: dict[str, Any],
+    ) -> None:
+        assert len(objects) == 1
+        tool = objects[0]
+
+        if tool.name == "utensil":
+            
+            print("In LookAtPlateHLA")
+            assert self._sim.held_object_name == "utensil"
+
+            # stop the keep horizontal thread (incase we're trying to re-acquire a bite)
+            if self.wrist_controller is not None:
+                self.wrist_controller.stop_horizontal_spoon_thread()
+
+            self.move_to_joint_positions(self._sim.scene_description.above_plate_pos)
+            
+            if self.wrist_controller is not None:
+                self.wrist_controller.set_velocity_mode()
+                self.wrist_controller.reset()
+
+            if self.flair is not None:
+
+                if self._robot_interface is not None:
+                    # Run FLAIR perception.
+                    camera_color_data, camera_info_data, camera_depth_data, _ = (
+                        self._perception_interface.get_camera_data()
+                    )
+                    # log the data
+                    if self.log_path is not None:
+                        # save the data
+                        acq_data = {
+                            "camera_color_data": camera_color_data,
+                            "camera_info_data": camera_info_data,
+                            "camera_depth_data": camera_depth_data,
+                        }
+                        with open(self.log_path / "acq_data.pkl", "wb") as f:
+                            pickle.dump(acq_data, f)
+                else:
+                    # read last logged data
+                    try:
+                        with open(self.log_path / "acq_data.pkl", "rb") as f:
+                            acq_data = pickle.load(f)
+                        camera_color_data = acq_data["camera_color_data"]
+                        camera_info_data = acq_data["camera_info_data"]
+                        camera_depth_data = acq_data["camera_depth_data"]
+                    except FileNotFoundError:
+                        raise FileNotFoundError("No logged data found")
+
+                items = self.flair.identify_plate(camera_color_data)
+                # flair.set_food_items(items)
+                # self.flair.set_food_items(['banana', 'baby carrot'])
+                self.flair.set_food_items(['cantaloupe', 'banana'])
+                # self.flair.set_food_items(['chicken nugget'])
+                # self.flair.set_food_items(['banana'])
+                items_detection = self.flair.detect_items(camera_color_data, camera_depth_data, camera_info_data, log_path=None)
+                
+                if not self.flair.is_preference_set():
+
+                    # Handle one-time preference setting.
+    
+                    food_type_to_data = items_detection['food_type_to_bounding_boxes_plate']
+                    n_food_types = len(food_type_to_data)
+                    data = [{k: v} for k, v in food_type_to_data.items()]
+
+                    food_types = food_type_to_data.keys()
+
+                    # TODO: generalize this...
+                    ordering_options = [f"Eat all the {food_type}s first" for food_type in food_types]
+                    ordering_options += ["No preference"]
+
+                    # # save plate image, plate bounds, and original image pickle
+                    # import pickle
+                    # with open("plate_log.pkl", "wb") as f:
+                    #     plate_log = {
+                    #         "plate_image": items_detection['plate_image'],
+                    #         "plate_bounds": items_detection['plate_bounds'],
+                    #         "original_image": camera_color_data,
+                    #     }
+                    #     pickle.dump(plate_log, f)
+
+                    if self._web_interface is not None:
+                        self._web_interface.send_web_interface_message({"state": "prepare_bite", "status": "completed"})
+                        time.sleep(0.2) # simulate delay for web interface
+                        self._web_interface.send_web_interface_image(items_detection['plate_image'])
+                        self._web_interface.send_web_interface_message({"n_food_types": n_food_types, "data": data})
+                        self._web_interface.send_web_interface_message({"n_ordering": len(ordering_options), "data": ordering_options})
+
+                        # save all of this in a pickle file:
+                        # import pickle
+                        # with open("meal_start_log.pkl", "wb") as f:
+                        #     meal_start_log = {
+                        #         "plate_image": items_detection['plate_image'],
+                        #         "plate_bounds": items_detection['plate_bounds'],
+                        #         "food_type_to_data": food_type_to_data,
+                        #         "ordering_options": ordering_options,
+                        #     }
+                        #     pickle.dump(meal_start_log, f)
+
+                        # self._web_interface.send_web_interface_message({"state": "prepare_bite", "status": "completed"})
+                        # time.sleep(1.0) # simulate delay, also needed for web interface
+                        # self._web_interface.update_web_interface_image(items_detection['plate_image'])
+                        # time.sleep(1.0)  # simulate delay, also needed for web interface
+                        # self._web_interface.send_web_interface_message({"n_food_types": n_food_types, "data": data})
+                        # self._web_interface.send_web_interface_message({"n_ordering": len(ordering_options), "data": ordering_options})
+
+                        # Wait for web interface to report order selection.
+                        print("WAITING TO GET PREFERENCE")
+                        while self._web_interface.user_preference is None:
+                            time.sleep(1e-1)
+                        print("FINISHED GETTING PREFERENCES")
+                        print("User Preference:", self._web_interface.user_preference)
+                        self.flair.set_preference(self._web_interface.user_preference)
+                # else:
+                #     self._web_interface.send_web_interface_image(items_detection['plate_image'])
+                #     time.sleep(1.0)  # simulate delay, also needed for web interface
+
+                # Prepare for bite acquisition.
+                print("Doing Bite Acquisition")
+                self.wrist_controller.set_velocity_mode()
+                self.wrist_controller.reset()
+
+                next_action_prediction = self.flair.predict_next_action(camera_color_data, items_detection=None, log_path=None)
+
+                next_food_item = next_action_prediction['labels_list'][next_action_prediction['food_id']]
+                bite_mask_idx = next_action_prediction['bite_mask_idx']
+                print(" --- Next Food Item Prediction:", next_action_prediction['labels_list'][next_action_prediction['food_id']])
+                print(" --- Next Action Prediction:", next_action_prediction['action_type'])
+
+                # remove next_food_item from data
+                food_type_to_data = items_detection['food_type_to_bounding_boxes_plate']
+
+                n_food_types = len(food_type_to_data)
+                data = [{k: v} for k, v in food_type_to_data.items() if k != next_food_item]
+                current_bite = {next_food_item: food_type_to_data[next_food_item]}
+
+                if self._web_interface is not None:
+                    self._web_interface.send_web_interface_message({"state": "prepare_bite", "status": "completed"})
+                    time.sleep(0.2) # simulate delay, needed for web interface
+                    self._web_interface.send_web_interface_image(items_detection['plate_image'])
+                    time.sleep(0.1)
+                    self._web_interface.send_web_interface_message({"n_food_types": n_food_types, "data": data, "current_bite": current_bite})            
+
+                # import pickle
+                # with open("next_bite_log.pkl", "wb") as f:
+                #     next_bite_log = {
+                #         "plate_image": items_detection['plate_image'],
+                #         "plate_bounds": items_detection['plate_bounds'],
+                #         "food_type_to_data": food_type_to_data,
+                #         "next_food_item": next_food_item,
+                #     }
+                #     pickle.dump(next_bite_log, f)
+  
+            else:
+                # Test image.
+                rng = np.random.default_rng(123)
+                camera_color_data = rng.integers(0, 255, size=(512, 512, 3))
+
+        else:
+            # Other tools are always prepared
+            pass
+        return []
+
+class AcquireBiteHLA(HighLevelAction):
+    """Bite acquisition; other tools are always prepared."""
+
+    def get_name(self) -> str:
+        return "AcquireBite"
+
+    def get_operator(self) -> LiftedOperator:
+        tool = Variable("?tool", tool_type)
+        return LiftedOperator(
+            self.get_name(),
+            parameters=[tool],
+            preconditions={Holding([tool]), IsUtensil([tool]), LiftedAtom(PlateInView, [])},
+            add_effects={ToolPrepared([tool])},
+            delete_effects={LiftedAtom(PlateInView, [])},
+        )
+
+    def execute_action(
+        self,
+        objects: tuple[Object, ...],
+        params: dict[str, Any],
+    ) -> None:
+        assert len(objects) == 1
+        tool = objects[0]
+
+        if tool.name == "utensil":
+
+            if self.flair is not None:
+
+                print("Doing Bite Acquisition")
+                camera_color_data, camera_info_data, camera_depth_data, _ = (
+                    self._perception_interface.get_camera_data()
+                )
+
+                if params["status"] == 0:
+
+                    detections = self.flair.get_items_detection()
+                    plate_bounds = detections["plate_bounds"]
+                    pos = params["positions"][0]
+
+                    point_x = int(pos["x"]*plate_bounds[2]) + plate_bounds[0]
+                    point_y = int(pos["y"]*plate_bounds[3]) + plate_bounds[1]
+
+                    print("Plate Bounds:", plate_bounds)
+                    print("Positions:", params["positions"])
+                    print("Point:", point_x, point_y)
+
+                    if not self.no_waits:
+                        # visualize point on camera color image
+                        viz = camera_color_data.copy()
+                        for pos in params["positions"]:
+                            cv2.circle(viz, (point_x, point_y), 5, (0, 255, 0), -1)
+                        cv2.imshow("viz", viz)
+                        cv2.waitKey(0)
+                        cv2.destroyAllWindows()
+
+                    skewer_center = (point_x, point_y)
+                    skewer_angle = -np.pi/2
+
+                    self.flair.skill_library.skewering_skill(camera_color_data, camera_depth_data, camera_info_data, keypoint = skewer_center, major_axis = skewer_angle)
+
+                elif params["status"] == "aquire_food":
+                    detections = self.flair.get_items_detection()
+                    food_type_to_masks = detections["food_type_to_masks"]
+                    food_type_to_skill = detections["food_type_to_skill"]
+                    
+                    food_type = params["data"][0]
+                    item_id = params["data"][1] - 1
+
+                    mask = food_type_to_masks[food_type][item_id]
+                    skill = food_type_to_skill[food_type]
+
+                    if skill == "Skewer":
+                        skewer_point, skewer_angle = self.flair.inference_server.get_skewer_action(mask)
+                        self.flair.skill_library.skewering_skill(camera_color_data, camera_depth_data, camera_info_data, keypoint = skewer_point, major_axis = skewer_angle)
+                    elif skill == "Scoop":
+                        raise NotImplementedError("Scoop skill not yet implemented")
+
+                self.move_to_joint_positions(self._sim.scene_description.above_plate_pos)
+
+                # set the wrist controller to always keep utensil horizontal
+                if self.wrist_controller is not None:
+                    self.wrist_controller.start_horizontal_spoon_thread()
+    
+            else:
+                time.sleep(2.0)  # simulate delay, also needed for web interface
+
+            # Send message to web interface indicating that robot is done with acquisition.
+            if self._web_interface is not None:
+                self._web_interface.send_web_interface_message({"state": "bite_pickup", "status": "completed"})
+
+            return []
+
+        else:
+            # Other tools are always prepared
+            pass
+        return []
--- src/feeding_deployment/simulation/control.py
+++ src/feeding_deployment/simulation/control.py
@@ -2,21 +2,13 @@
 
 from __future__ import annotations
 
-import logging
-from typing import Callable
-
 import numpy as np
-import time
 from scipy.linalg import cho_factor, cho_solve
 from scipy.spatial.transform import Rotation as R
 from scipy.spatial.transform import Slerp
 
 from pybullet_helpers.geometry import Pose
-from pybullet_helpers.gui import visualize_pose
 from pybullet_helpers.joint import JointPositions
-
-from feeding_deployment.simulation.simulator import FeedingDeploymentPyBulletSimulator
-from feeding_deployment.simulation.state import FeedingDeploymentSimulatorState
 
 # Rajat ToDo: Use a single yaml file to store all the constants across sim and real robot
 DAMPING_FACTOR = 0.05
@@ -24,118 +16,82 @@ DISTANCE_LOOKAHEAD = 0.04
 ANGULAR_LOOKAHEAD = 5*np.pi/180
 TIMESTEP = 1/240 # Default timestep in pybullet
 
-def _get_joint_trajectory_to_pose(
-    sim: FeedingDeploymentPyBulletSimulator,
-    target_pose: Pose,
-    max_control_time: float,
-) -> list[JointPositions]:
-    """
-    Returns a joint trajectory to move the robot to a target pose as the actual robot controller would.
-    """
+def cartesian_control_step(current_joint_positions: JointPositions, current_jacobian: np.ndarray, current_pose: Pose, target_pose: Pose) -> JointPositions:
 
-    # visualize the target pose
-    visualize_pose(target_pose, sim.physics_client_id)
-    visualize_pose(sim.robot.get_end_effector_pose(), sim.physics_client_id)
+    source_position = np.array(current_pose.position)
+    source_orientation = R.from_quat(current_pose.orientation)
+    target_position = np.array(target_pose.position)
+    target_orientation = R.from_quat(target_pose.orientation)
+
+    position_error = np.linalg.norm(source_position - target_position)
+    orientation_error = np.linalg.norm(R.from_matrix(np.dot(source_orientation.as_matrix(), target_orientation.as_matrix().T)).as_rotvec())
+
+    if position_error <= DISTANCE_LOOKAHEAD:
+        target_waypoint_position = target_position
+    else:
+        target_waypoint_position = source_position + DISTANCE_LOOKAHEAD*(target_position - source_position)/position_error
+
+    if orientation_error <= ANGULAR_LOOKAHEAD:
+        target_waypoint_orientation = target_orientation.as_quat()
+    else:
+        key_times = [0, 1]
+        key_rots = R.concatenate((source_orientation, target_orientation))
+        slerp = Slerp(key_times, key_rots)
+
+        interp_rotations = slerp([ANGULAR_LOOKAHEAD/orientation_error]) #second last is also aligned
+        target_waypoint_orientation = interp_rotations[0].as_quat()
+
+    # visualize_pose(current_pose, sim.physics_client_id)
+    # visualize_pose(Pose(position=target_waypoint_position, orientation=target_waypoint_orientation), sim.physics_client_id)
     # input("Press Enter to continue...")
-        
-    # Rajat ToDo: make sure the controller is working
 
-    def compute_next_step(sim, target_pose):
-        current_pose = sim.robot.get_end_effector_pose()
+    n_dof = 7 # Rajat ToDo: Remove hardcoding
 
-        source_position = np.array(current_pose.position)
-        source_orientation = R.from_quat(current_pose.orientation)
-        target_position = np.array(target_pose.position)
-        target_orientation = R.from_quat(target_pose.orientation)
+    J = current_jacobian
+    # print("J.shape", J.shape)
+    J = J[:, :n_dof]
 
-        position_error = np.linalg.norm(source_position - target_position)
-        orientation_error = np.linalg.norm(R.from_matrix(np.dot(source_orientation.as_matrix(), target_orientation.as_matrix().T)).as_rotvec())
+    pos_error = source_position - target_waypoint_position
 
-        if position_error <= DISTANCE_LOOKAHEAD:
-            target_waypoint_position = target_position
-        else:
-            target_waypoint_position = source_position + DISTANCE_LOOKAHEAD*(target_position - source_position)/position_error
+    # Convert to Rotation objects
+    R_c = R.from_quat(current_pose.orientation)
+    R_d = R.from_quat(target_waypoint_orientation)
 
-        if orientation_error <= ANGULAR_LOOKAHEAD:
-            target_waypoint_orientation = target_orientation.as_quat()
-        else:
-            key_times = [0, 1]
-            key_rots = R.concatenate((source_orientation, target_orientation))
-            slerp = Slerp(key_times, key_rots)
+    # Adjust quaternions to be on the same hemisphere
+    if np.dot(R_d.as_quat(), R_c.as_quat()) < 0.0:
+        R_c = R.from_quat(-R_c.as_quat())
 
-            interp_rotations = slerp([ANGULAR_LOOKAHEAD/orientation_error]) #second last is also aligned
-            target_waypoint_orientation = interp_rotations[0].as_quat()
+    # Compute error rotation
+    error_rotation = R_c.inv() * R_d
 
-        # visualize_pose(current_pose, sim.physics_client_id)
-        # visualize_pose(Pose(position=target_waypoint_position, orientation=target_waypoint_orientation), sim.physics_client_id)
-        # input("Press Enter to continue...")
+    # Convert error rotation to quaternion
+    error_quat = error_rotation.as_quat()
 
-        n_dof = 7 # Rajat ToDo: Remove hardcoding
+    # Extract vector part
+    orient_error_vector = error_quat[:3]
 
-        # Use damped least squares to compute the joint velocities
-        J = sim.robot.get_jacobian()
-        # print("J.shape", J.shape)
-        J = J[:, :n_dof]
+    # Get rotation matrix of nominal pose
+    R_c_matrix = R_c.as_matrix()
 
-        pos_error = source_position - target_waypoint_position
+    # Compute orientation error
+    orient_error = -R_c_matrix @ orient_error_vector
 
-        # Convert to Rotation objects
-        R_c = R.from_quat(current_pose.orientation)
-        R_d = R.from_quat(target_waypoint_orientation)
+    # Assemble error
+    error = np.zeros(6)
+    error[:3] = pos_error
+    error[3:] = orient_error 
 
-        # Adjust quaternions to be on the same hemisphere
-        if np.dot(R_d.as_quat(), R_c.as_quat()) < 0.0:
-            R_c = R.from_quat(-R_c.as_quat())
+    damping_lambda = DAMPING_FACTOR * np.eye(n_dof)
+    J_JT = J.T @ J + damping_lambda
+            
+    # J_damped = np.linalg.inv(J_JT) @ J.T
 
-        # Compute error rotation
-        error_rotation = R_c.inv() * R_d
+    # this is faster than the above commented computation
+    c, lower = cho_factor(J_JT)
+    J_n_damped = cho_solve((c, lower), J.T)
 
-        # Convert error rotation to quaternion
-        error_quat = error_rotation.as_quat()
+    joint_velocities = -J_n_damped @ error
 
-        # Extract vector part
-        orient_error_vector = error_quat[:3]
+    target_positions = current_joint_positions[:n_dof] + joint_velocities * TIMESTEP
 
-        # Get rotation matrix of nominal pose
-        R_c_matrix = R_c.as_matrix()
-
-        # Compute orientation error
-        orient_error = -R_c_matrix @ orient_error_vector
-
-        # Assemble error
-        error = np.zeros(6)
-        error[:3] = pos_error
-        error[3:] = orient_error 
-
-        damping_lambda = DAMPING_FACTOR * np.eye(n_dof)
-        J_JT = J.T @ J + damping_lambda
-                
-        # J_damped = np.linalg.inv(J_JT) @ J.T
-
-        # this is faster than the above commented computation
-        c, lower = cho_factor(J_JT)
-        J_n_damped = cho_solve((c, lower), J.T)
-
-        joint_velocities = -J_n_damped @ error
-
-        target_positions = sim.robot.get_joint_positions()[:n_dof] + joint_velocities * TIMESTEP
-
-        return target_positions
-    
-    joint_positions: list[JointPositions] = []
-        
-    start_time = time.time()
-    target_reached = False
-    while time.time() - start_time < max_control_time:
-        if target_pose.allclose(sim.robot.get_end_effector_pose(), atol=1e-2):
-            target_reached = True
-            break
-        joint_positions.append(sim.robot.get_joint_positions())
-        target_positions = compute_next_step(sim, target_pose)
-        target_positions = np.concatenate((target_positions, sim.robot.finger_state_to_joints(sim.scene_description.tool_grasp_fingers_value))) # Rajat ToDo: Remove hardcoding
-        sim.set_robot_motors(target_positions)
-    
-    if not target_reached:
-        raise RuntimeError("Sim cartesian controller: Failed to reach target pose in time")
-
-    return joint_positions
+    return target_positions
